ea325c6da0d58434bc6ab7b71548ba6cb7b7bfad
Xin <73196629+Apricita@users.noreply.github.com>
2021-10-16 21:15:22 -0600

    Fixed an error. Updated UI.

--- LinearRegression/LMS.py
+++ LinearRegression/LMS.py
@@ -13,6 +13,15 @@ def load_csv(filepath):
     y = dataset[outcome]
     return x, y
 
+## define cost function
+def cost(x, y, w):
+    m = y.shape[0]
+    prediction = np.dot(x, w)
+    error = prediction - y['Slump']
+    j = 1 / (2 * m) * np.dot(error.T, error)
+    return error, j
+
+## build gradient descent algorithm
 def gradient_descent(x, y, alpha, tolerance, max_iter):
     m = y.shape[0]
     w = [0] * x.shape[1]
@@ -22,7 +31,7 @@ def gradient_descent(x, y, alpha, tolerance, max_iter):
     iteration = 1
     while converge >= tolerance:
         if iteration >= max_iter:
-            print('Warning: Model connot converge. Reached max iteration.')
+            print('Warning: Max iteration reached. Consider adjusting learning rate or increase max iteration')
             break
         error, j = cost(x, y, w)
         past_j.append(j)
@@ -31,19 +40,16 @@ def gradient_descent(x, y, alpha, tolerance, max_iter):
         past_w.append(w)
         converge = np.sqrt(np.dot(gradient.T, gradient))
         iteration += 1
-    return past_w, past_j
+    return past_w, past_j, iteration
 
+## build stochastic gradient descent algorithm
 def stochastic_gradient_descent(x, y, alpha, tolerance, max_iter):
     m = y.shape[0]
     w = [0] * x.shape[1]
     past_j = []
     past_w = [w]
-    converge = 999
     iteration = 1
-    while converge >= tolerance:
-        if iteration >= max_iter:
-            print('Warning: Model connot converge. Reached max iteration.')
-            break
+    for _ in range(max_iter):
         rand_ind = np.random.choice(range(m), m, replace=False)
         for ind in rand_ind:
             error, j = cost(x, y, w)
@@ -52,67 +58,51 @@ def stochastic_gradient_descent(x, y, alpha, tolerance, max_iter):
             w = w - gradient
             past_w.append(w)
             converge = np.sqrt(np.dot(gradient.T, gradient))
-            if converge >= tolerance:
+            if converge < tolerance:
                 break
         iteration += 1
-    return past_w, past_j
+    return past_w, past_j, iteration
 
-def cost(x, y, w):
-    m = y.shape[0]
-    prediction = np.dot(x, w)
-    error = prediction - y['Slump']
-    j = 1 / (2 * m) * np.dot(error.T, error)
-    return error, j
-
-def driver(x, y, r, tol, max_iteration, algorithm):
-    w_list, j_list = algorithm(x, y, r, tol, max_iteration)
+## build driver to run algorithm and return analyzed results
+def driver(train_x, train_y, test_x, test_y, r, tol, max_iteration, algorithm):
+    w_list, j_list, iteration = algorithm(train_x, train_y, r, tol, max_iteration)
 
     ## adjust learning rate
     while any(j >= 100 for j in j_list):
-        print('Adjusting learning rate...')
+        print('Cost is increasing. Decreasing learning rate and trying again...')
         r = r / 2
-        w_list, j_list = gradient_descent(train_x, train_y, r, tol, max_iteration)
-    while len(w_list) >= max_iteration:
-        print('Adjusting learning rate...')
-        r = r * 1.5
-        w_list, j_list = gradient_descent(train_x, train_y, r, tol, max_iteration)
-    return w_list, j_list, r
-
-
-if __name__ == '__main__':
-    train_file = sys.argv[1]
-    test_file = sys.argv[2]
-    train_x, train_y = load_csv(train_file)
-    test_x, test_y = load_csv(test_file)
-    try:
-        r = float(sys.argv[3])
-        tol = float(sys.argv[4])
-        max_iteration = int(sys.argv[5])
-    except IndexError:
-        r = 1.0
-        tol = 1e-6
-        max_iteration = 20000
-
-    print('Using gradient descent...')
-    w_list, j_list, r = driver(train_x, train_y, r, tol, max_iteration, gradient_descent)
+        w_list, j_list = algorithm(train_x, train_y, r, tol, max_iteration)
+    if iteration >= max_iteration:
+        print('Warning: Max iteration reached. Consider adjusting learning rate or increase max iteration')
     final_w = w_list[-1]
     test_error, test_j = cost(test_x, test_y, final_w)
-    print(f'Converged at iteration {len(j_list)}')
+    print(f'Stopped at iteration {iteration}')
     print(f'Cost of the test set is {round(test_j, 3)}')
     print(f'Coefficients are {final_w}')
     print(f'Learning rate is {r}')
     plot_sel = input('Do you want to plot cost trajectory? (Y/N)')
     if plot_sel == 'Y':
         plot_regressor(range(len(j_list)), j_list)
+    return
 
-    print('Using stochastic gradient descent...')
-    w_list2, j_list2, r2 = driver(train_x, train_y, r, tol, max_iteration, stochastic_gradient_descent)
-    final_w2 = w_list2[-1]
-    test_error2, test_j2 = cost(test_x, test_y, final_w)
-    print(f'Converged at iteration {len(j_list2)}')
-    print(f'Cost of the test set is {round(test_j2, 3)}')
-    print(f'Coefficients are {final_w2}')
-    print(f'Learning rate is {r2}')
-    plot_sel2 = input('Do you want to plot cost trajectory? (Y/N)')
-    if plot_sel2 == 'Y':
-        plot_regressor(range(len(j_list2)), j_list2)
+
+if __name__ == '__main__':
+    train_file = sys.argv[1]
+    test_file = sys.argv[2]
+    alg = sys.argv[3]
+    train_x, train_y = load_csv(train_file)
+    test_x, test_y = load_csv(test_file)
+    try:
+        r = float(sys.argv[4])
+        tol = float(sys.argv[5])
+        max_iteration = int(sys.argv[6])
+    except IndexError:
+        r = 1.0
+        tol = 1e-6
+        max_iteration = 20000
+
+    print(f'Using {alg}...')
+    if alg == 'gradient_descent':
+        driver(train_x, train_y, test_x, test_y, r, tol, max_iteration, gradient_descent)
+    if alg == 'stochastic_gradient_descent':
+        driver(train_x, train_y, test_x, test_y, r, tol, max_iteration, stochastic_gradient_descent)
