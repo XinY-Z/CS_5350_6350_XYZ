dc2f2f1446d871d44faf12d1f7afe92ea15cf84f
Xin <73196629+Apricita@users.noreply.github.com>
2021-11-05 00:51:11 -0600

    Improved operability.

--- FinalProject/LanguageModel/Preprocessor.py
+++ FinalProject/LanguageModel/Preprocessor.py
@@ -3,8 +3,8 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm')
 
-class Preprocessor(spacy):
-    def __init__(self, lowercase, lemma, remove_punc, remove_stopwords):
+class Preprocessor:
+    def __init__(self, lowercase=True, lemma=True, remove_punc=True, remove_stopwords=False):
         super().__init__()
         self.__lowercase = lowercase
         self.__lemma = lemma
@@ -12,17 +12,17 @@ class Preprocessor(spacy):
         self.__remove_stopwords = remove_stopwords
 
     # remove punctuation and stopwords, lowercase the words and lemmatize
-    def preprocessor(self, string):
-        if self.lowercase:
+    def preprocess(self, string):
+        if self.__lowercase:
             string = string.lower()
         doc = nlp(string)
 
-        if self.remove_punc:
+        if self.__remove_punc:
             doc = [token for token in doc if not token.is_punct]
-        if self.remove_stopwords:
+        if self.__remove_stopwords:
             doc = [token for token in doc if not token.is_stop]
 
-        if self.lemma:
+        if self.__lemma:
             doc = [token.lemma_ for token in doc]
         else:
             doc = [token.text for token in doc]
--- FinalProject/LanguageModel/TfIdf.py
+++ FinalProject/LanguageModel/TfIdf.py
@@ -5,7 +5,7 @@ from FinalProject.LanguageModel.Preprocessor import Preprocessor
 from FinalProject.DataLoader import DataLoader
 
 
-dataloader = DataLoader(r'C:\Users\XinZ\Box\SafeUT_Data\Final_Anonymization\FINAL_ANONYMIZED_SAFEUT.xlsx')
+dataloader = DataLoader(r'C:\Users\XinZ\Box\SafeUT_Data\Final_Anonymization\FINAL_ANONYMIZED_SAFEUT.xlsx', sheet='Message 1')
 dataloader.random_select(500)
 dataloader.to_dropout(6)
 preprocessor = Preprocessor(lowercase=True, lemma=True, remove_punc=True, remove_stopwords=False)
@@ -26,7 +26,8 @@ class Vectorizer(TfidfVectorizer):
 
         ## remove auto-generated messages
         self.input = self.input[self.input['originator'] != 'Auto']
-        data_sel = self.input.groupby('encounterId').head(self.__nmessage)[['messageId', 'encounterId', 'originator', 'message', 'dropout']]
+        data_sel = self.input.groupby('encounterId').head(self.__nmessage)
+        data_sel = data_sel.loc[:, ['messageId', 'encounterId', 'originator', 'message', 'dropout']]
         data_sel['message'] = data_sel['message'].apply(lambda x: str(x))
         doclist = data_sel.groupby('encounterId')['message'].apply(list).to_list()
         doclist = [' '.join(doc) for doc in doclist]
@@ -40,9 +41,9 @@ class Vectorizer(TfidfVectorizer):
 
         ## instantiate tf-idf vectorizer
         if isinstance(self.__ngram, int):
-            tfidf = TfidfVectorizer(ngram_range=(self.__ngram, self.__ngram), preprocessor=preprocessor.preprocessor)
+            tfidf = TfidfVectorizer(ngram_range=(self.__ngram, self.__ngram), preprocessor=preprocessor.preprocess)
         elif isinstance(self.__ngram, tuple):
-            tfidf = TfidfVectorizer(ngram_range=(self.__ngram[0], self.__ngram[1]), preprocessor=preprocessor.preprocessor)
+            tfidf = TfidfVectorizer(ngram_range=(self.__ngram[0], self.__ngram[1]), preprocessor=preprocessor.preprocess)
         else:
             raise TypeError('Argument "ngram" must be int or tuple.')
 
