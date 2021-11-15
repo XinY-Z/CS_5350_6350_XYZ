97173d162175a06b9f99ef795f6e6bebe75f8ece
Xin <73196629+Apricita@users.noreply.github.com>
2021-11-14 20:28:14 -0700

    Fixed bugs

--- Perceptron/run.sh
+++ Perceptron/run.sh
@@ -1,1 +1,3 @@
-Python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv perceptron
+python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv perceptron
+python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv voted_perceptron
+python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv averaged_perceptron
